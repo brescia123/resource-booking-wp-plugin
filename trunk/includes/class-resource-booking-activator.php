e4bfb17fdf305dc9bf0a8debd8d1d2fbde4b3db4
Giacomo Bresciani <brescia123@gmail.com>
2015-02-17 11:19:29 +0100

    create reservation table

--- trunk/includes/class-resource-booking-activator.php
+++ trunk/includes/class-resource-booking-activator.php
@@ -30,7 +30,25 @@ class Resource_Booking_Activator {
 	 * @since    0.1.0
 	 */
 	public static function activate() {
+		global $wpdb;
 
+		// Create reservation table
+		$reservation_table_name = $wpdb->prefix . "reservations";
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $reservation_table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			resource_id mediumint(9) NOT NULL,
+			start datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			end datetime DEFAULT '0000-00-00 00:00:00' NOT NULL, 
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		// Compare with db if there are differences executes the sql
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 	}
 
 }

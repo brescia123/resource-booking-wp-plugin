ddff35bf24c38ece03530b7a54b10ab61a73f1bb
Giacomo Bresciani <brescia123@gmail.com>
2015-03-20 10:44:02 +0100

    calendar reflects slotDuration

--- trunk/admin/js/resource-booking-admin.js
+++ trunk/admin/js/resource-booking-admin.js
@@ -5,36 +5,46 @@
     $(function() {
         var calendar = $('#calendar');
         var post_id = $('#post_ID').val();
-        var time_interval = '00:' + $('#time-interval').find('option:selected').val() + ':00';
+        var time_interval_select = $('#time-interval');
+        var time_interval = '00:' + time_interval_select.find('option:selected').val() + ':00';
 
         // Calendar configuration
-        calendar
-            .fullCalendar({
-                header: {
-                    left: 'title',
-                    center: 'agendaDay,agendaWeek,month',
-                    right: 'today prev,next'
-                },
-                weekends: false,
-                defaultView: 'agendaWeek',
-                allDaySlot: false,
-                editable: true,
-                slotDuration: time_interval,
-                events: function(start, end, timezone, callback) {
-                    var data = {
-                        'action': 'res_reservations_callback',
-                        'res_id': post_id,
-                        'start_date': start.format(),
-                        'end_date': end.format(),
-                    };
-                    jQuery.post(ajax_object.ajax_url, data, function(response) {
-                        var reservations = JSON.parse(response);
-                        var events = resToEventsArray(reservations)
-                        console.log(events);
-                        callback(events);
-                    });
+        var calendarOpts = {
+            header: {
+                left: 'title',
+                center: 'agendaDay,agendaWeek,month',
+                right: 'today prev,next'
+            },
+            weekends: false,
+            defaultView: 'agendaWeek',
+            allDaySlot: false,
+            editable: true,
+            slotDuration: time_interval,
+            events: function(start, end, timezone, callback) {
+                var data = {
+                    'action': 'res_reservations_callback',
+                    'res_id': post_id,
+                    'start_date': start.format(),
+                    'end_date': end.format(),
                 }
-            });
+                jQuery.post(ajax_object.ajax_url, data, function(response) {
+                    var reservations = JSON.parse(response);
+                    var events = resToEventsArray(reservations)
+                    console.log(events);
+                    callback(events);
+                });
+            }
+        };
+        calendar.fullCalendar(calendarOpts);
+
+        time_interval_select.change(function () {
+            var minutesTot = $(this).val();
+            var hours = parseInt(minutesTot / 60);
+            var minutesRel = minutesTot % 60;
+            calendarOpts.slotDuration = '00:' + minutesTot + ':00';
+            calendar.fullCalendar('destroy');
+            calendar.fullCalendar(calendarOpts);
+        })
 
         var resToEventsArray = function(reservations) {
             var events = [];

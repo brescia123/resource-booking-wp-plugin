c014550a392c2d7b44ff2f7bd9d9fa5a95c16e11
Giacomo Bresciani <brescia123@gmail.com>
2015-04-17 16:13:56 +0200

    implemented front-end

--- trunk/public/class-resource-booking-public.php
+++ trunk/public/class-resource-booking-public.php
@@ -22,6 +22,12 @@
  */
 class Resource_Booking_Public {
 
+	/*
+	Constants
+	*/
+	const FULLCALENDAR_VERSION = '2.2.6';
+	const MOMENT_VERSION = '2.9.0';
+
 	/**
 	 * The ID of this plugin.
 	 *
@@ -40,6 +46,8 @@ class Resource_Booking_Public {
 	 */
 	private $version;
 
+	private $rb_db;
+
 	/**
 	 * Initialize the class and set its properties.
 	 *
@@ -51,6 +59,7 @@ class Resource_Booking_Public {
 
 		$this->Resource_Booking = $Resource_Booking;
 		$this->version = $version;
+		$this->rb_db = new Resource_Booking_DB();
 
 	}
 
@@ -73,7 +82,37 @@ class Resource_Booking_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->Resource_Booking, plugin_dir_url( __FILE__ ) . 'css/resource-booking-public.css', array(), $this->version, 'all' );
+		global $post;
+		if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'resource_booking') ) {		
+			wp_enqueue_style( 'wp-jquery-ui-dialog' );
+			wp_enqueue_style( 'fullcalendar', 'http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/'.self::FULLCALENDAR_VERSION.'/fullcalendar.min.css', array(), self::FULLCALENDAR_VERSION, 'all' );
+		}
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    0.1.0
+	 */
+	public function enqueue_styles_fix() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Resource_Booking_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Resource_Booking_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		global $post;
+		if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'resource_booking') ) {		
+			wp_enqueue_style( $this->Resource_Booking, plugin_dir_url( __FILE__ ) . 'css/resource-booking-public.css', array(), $this->version, 'all' );
+		}
 
 	}
 
@@ -96,8 +135,15 @@ class Resource_Booking_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->Resource_Booking, plugin_dir_url( __FILE__ ) . 'js/resource-booking-public.js', array( 'jquery' ), $this->version, false );
-
+		// Add script to the page only if there is the plugin shortcode
+		global $post;
+		if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'resource_booking') ) {
+			wp_enqueue_script( $this->Resource_Booking, plugin_dir_url( __FILE__ ) . 'js/resource-booking-public.js', array( 'jquery' ), $this->version, false );
+			// in JavaScript, object properties are accessed as ajax_object.*
+			wp_localize_script( $this->Resource_Booking, 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) );
+			wp_enqueue_script( 'momentjs', 'http://cdnjs.cloudflare.com/ajax/libs/moment.js/'.self::MOMENT_VERSION.'/moment.min.js', array( 'jquery' ), self::MOMENT_VERSION, true );
+			wp_enqueue_script( 'fullcalendar', 'http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/'.self::FULLCALENDAR_VERSION.'/fullcalendar.min.js', array( 'jquery', 'momentjs' ), self::FULLCALENDAR_VERSION, true );
+		}	
 	}
 
 	/**
@@ -112,7 +158,38 @@ class Resource_Booking_Public {
         	'resource_id' => '0',
     	), $atts ) );
 
-		return $resource_id;
+		$resource = get_post($resource_id);
+		if( !$resource ) {
+			return '<h4>The resource '.$resource_id.' is no more available. Please contact the site administrator.</h4>';
+		}
+
+		// Get the time-interval value
+		$selected_time_interval = get_post_meta( $resource_id, Resource_Booking_Res_Mb::TIME_INTERVAL_MK, true );
+		if( !$selected_time_interval ){
+			$selected_time_interval = 30;
+		}
+
+		// Get the color value
+		$selected_res_color = get_post_meta( $resource_id, Resource_Booking_Res_Mb::RES_COLOR_MK, true );
+		if( !$selected_res_color ){
+			$selected_res_color = '#7bd148';
+		}
+
+
+		$contents = '<script type="text/javascript">
+						if(typeof resourcesInfo === "undefined"){
+							resourcesInfo = {};
+						}
+						resourcesInfo["'.$resource_id.'"] = {
+				    		"timeInterval": "'.$selected_time_interval.'",
+				    		"resColor": "'.$selected_res_color.'"
+						};
+					</script>';
+
+		$contents = $contents.'<h2>'.$resource->post_title.'</h2>';
+		$contents = $contents.'<div id="'.$resource_id.'" class="calendar"></div>';
+
+		return $contents;
 
 	}
 }
--- trunk/public/js/resource-booking-public.js
+++ trunk/public/js/resource-booking-public.js
@@ -1,6 +1,114 @@
 (function($) {
     'use strict';
 
+    // Document ready
+    $(function() {
+
+        $('.calendar').each(function() {
+
+            var resourceId = this.id;
+            var timeInterval = '00:' + resourcesInfo[resourceId].timeInterval + ':00';
+            var resColor = resourcesInfo[resourceId].resColor;
+
+            // Calendar configuration
+            var calendarOpts = {
+                header: {
+                    left: 'title',
+                    center: 'agendaDay,agendaWeek,month',
+                    right: 'today prev,next'
+                },
+                weekends: false,
+                defaultView: 'agendaWeek',
+                allDaySlot: false,
+                eventColor: resColor,
+                selectable: true,
+                selectOverlap: false,
+                eventOverlap: false,
+                editable: false,
+                resizable: false,
+                slotDuration: timeInterval,
+                views: {
+                    week: {
+                        titleFormat: 'MMMM D YYYY'
+                    }
+                },
+                eventSources: [{
+                    // Get events from the db
+                    events: getEvents
+                }],
+                select: newEvent,
+                // eventClick: showEventDialog
+            };
+            $(this).fullCalendar(calendarOpts);
+
+            // Makes a POST request to the server to get all the events in a given time interval
+            function getEvents(start, end, timezone, callback) {
+                var data = {
+                    'action': 'res_reservations_callback',
+                    'res_id': resourceId,
+                    'start': start.format(),
+                    'end': end.format(),
+                }
+                jQuery.post(ajax_object.ajax_url, data, function(response_json) {
+                    var response = JSON.parse(response_json);
+                    if (response.success) {
+                        var events = $.map(response.reservations, resToEvent);
+                        callback(events);
+                    } else {
+                        alert('Error: \n' + response_json);
+                    }
+                });
+            }
+
+            // Makes a POST request to the server for a new reservation
+            function newEvent(start, end, jsEvent, view) {
+                var title = prompt('Name:');
+                if (title) {
+                    var data = {
+                            'action': 'res_save_reservation_callback',
+                            'res_id': resourceId,
+                            'title': title,
+                            'start': start.format(),
+                            'end': end.format()
+                        }
+                        // Post the new event to the server
+                    jQuery.post(ajax_object.ajax_url, data, function(response_json) {
+                        var response = JSON.parse(response_json);
+
+                        if (response.success) {
+                            var event = resToEvent(response.reservation);
+                            $(this).fullCalendar('renderEvent', event, true);
+                        } else {
+                            alert('Error: \n' + response_json);
+                        }
+                    });
+                }
+            }
+
+        });
+
+        /* Helper Methods */
+
+        // Converts a reservation object retrieved from the server in FullCalendar event
+        var resToEvent = function(reservation) {
+            return {
+                'id': reservation.id,
+                'title': reservation.title,
+                'start': reservation.start,
+                'end': reservation.end,
+                'allDay': false
+            }
+        }
+
+        // Converts an envent to a reservation object
+        var eventToRes = function(event) {
+            return {
+                'id': postId,
+                'title': event.title,
+                'start': event.start.toString(),
+                'end': event.end.toString()
+            }
+        }
+    });
 
 })(jQuery);
-    
